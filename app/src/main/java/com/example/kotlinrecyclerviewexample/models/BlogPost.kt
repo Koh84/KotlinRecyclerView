c6b568f9af36ecf39d37753bdda3d61ef5fc00e9
Kelvin <koh84@hotmail.com>
2021-09-08 22:29:32 +0100

    Recycler view

---
+++ app/src/main/java/com/example/kotlinrecyclerviewexample/models/BlogPost.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinrecyclerviewexample.models
+
+data class BlogPost(
+    var title: String,
+    var body: String,
+    var image: String,
+    var username: String
+) {
+
+}
